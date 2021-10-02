079e000bc64300ccf7dfd5a7ca57aa12fa991dda
garrettkinman <garrettkinman@gmail.com>
2021-10-02 16:26:51 -0400

    created wrapper functions for noise, with randomized params

--- RLImageDenoiser.jl
+++ RLImageDenoiser.jl
@@ -5,6 +5,7 @@ using ReinforcementLearning
 using FileIO
 using Noise
 using Pipe
+using Combinatorics
 
 ## constants
 
@@ -29,12 +30,23 @@ const NOISY_IMG_PATH = "./images/noisy/"
 filenames = readdir(ORIG_IMG_PATH, join=true)
 orig_imgs = load.(filenames)
 
-## generate noisy images
+## declare wrapper functions
 
-orig_imgs[1]
-add_gauss(orig_imgs[1], clip=true)
-salt_pepper(orig_imgs[1])
-poisson(orig_imgs[1], 10, clip=true)
-quantization(orig_imgs[1], 10)
+# to make functional programming easier, we want to wrap the noise functions
+# so we can keep the correct kwargs,
+# randomly set σ for some of them,
+# and expose only one input (i.e., the image)
 
-@pipe orig_imgs[1] |> add_gauss(_, clip=true) |> salt_pepper |> poisson(_, 10, clip=true) |> quantization(_, 10)
+# use random σ (std dev) from 0 to 0.5
+add_gauss_wrapper(img::AbstractMatrix) = add_gauss(img, rand() / 2, clip=true)
+mult_gauss_wrapper(img::AbstractMatrix) = mult_gauss(img, rand() / 2, clip=true)
+salt_pepper_wrapper(img::AbstractMatrix) = salt_pepper(img, rand() / 2)
+
+# use random 10 to 100 photons
+poisson_wrapper(img::AbstractMatrix) = poisson(img, (abs(rand(Int)) % 90) + 10)
+
+# use random 5 to 20 levels
+quantization_wrapper(img::AbstractMatrix) = quantization(img, (abs(rand(Int)) % 10) + 10)
+
+## add random combinations of noise types to each image
+
